5aeda504759a99a69001daa966d6c03f43b0cd0d
Leon <leonbrcs@gmail.com>
2020-11-14 16:08:30 +0200

    strcpy example

--- _ex.cpp
+++ _ex.cpp
@@ -1,4 +1,34 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
 #include <iostream>
+#include <string>
 
 
-class 
+size_t slen(const char * str) {
+    size_t i = 0;
+    while (str[++i]);
+    return i;
+}
+
+
+void scpy(const char * src, char ** dest) {
+    size_t len = slen(src);
+    *dest = (char *)malloc(len + 1);
+    for (size_t i = 0; i < len + 1; i++)
+    {
+        (*dest)[i] = src[i];
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    const char * p = "test";
+    char * t;
+    scpy(p, &t);
+    std::cout << p << " " << t << std::endl;
+
+    return 0;
+}
