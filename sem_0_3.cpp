438e89dcbc84383625efc11b027961dac48230c6
Leon <leonbrcs@gmail.com>
2020-11-14 11:49:12 +0200

    20201114

--- sem_0_3.cpp
+++ sem_0_3.cpp
@@ -6,69 +6,97 @@
 #include <string>
 
 
-void memory(int *** a, int n, int m) {
-    *a = (int **)calloc(n, sizeof(int *));
-    for (size_t i = 0; i < n; i++)
-    {
-        (*a)[i] = (int *)calloc(m, sizeof(int));
-    }
-}
-
-
-void print(int ** a, int n, int m) {
-    for (size_t i = 0; i < n; i++)
-    {
-        for (size_t j = 0; j < m; j++)
-        {
-            printf("%d\t", a[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-
-struct M {
-    int n;
-    char * s;
-    std::string S;
+struct Example {
+    int number;
+    char * c_type_string;
+    std::string obj_string;
 };
 
 
-void struct_examples() {
-    M mys = {0, {}, ""};
+void memory(int *** array_pointer_reference, int rows, int columns);
+void clear(int ** array_pointer, int rows);
+void print(int ** array_pointer, int rows, int columns);
 
-    char buf[80];
-
-    std::cin >> mys.n >> buf >> mys.S;
-
-    size_t buflen = strlen(buf);
-
-    mys.s = (char *)malloc(buflen + 1);
-    strcpy(mys.s, buf);
-
-    std::cout << mys.n << "\t" << mys.s << "\t" << mys.S;
-}
+void struct_example();
+void menu_example();
 
 
 int main(int argc, char const *argv[])
 {
-    int ** A = NULL;
-    int N = 4, M = 4;
+    struct_example();
 
-    int option = -1;
+    std::cout << "------------------";
+
+    menu_example();
+
+    return 0;
+}
+
+void struct_example() 
+{
+    Example ex = {0, {}, ""};
+
+    char buf[80];
+
+    std::cin >> ex.number >> buf >> ex.obj_string;
+
+    size_t buflen = strlen(buf);
+
+    ex.c_type_string = (char *)malloc(buflen + 1);
+    strcpy(ex.c_type_string, buf);
+
+    std::cout << ex.number << "\t" << ex.c_type_string << "\t" << ex.obj_string;
+}
+
+void menu_example() 
+{
+    int ** matrix_pointer = NULL;
+    int rows_number = 4, columns_number = 4, option = -1;
 
     while(option)
     {
-        printf("press 1-7 for options:\n");
+        printf("press 1-6 to select corresponding action:\n1. ");
         scanf("%i", &option);
         if (!option) {
-            return 0;
-        } else if (!A && option == 1) {
-            memory(&A, N, M);
-        } else if (A && option == 2) {
-            print(A, N, M);
-        } else {
-            printf("no such option exists\n");
+            exit(0);
+        } else if (!matrix_pointer && option == 1) {
+            memory(&matrix_pointer, rows_number, columns_number);
+        } else if (matrix_pointer && option == 2) {
+            print(matrix_pointer, rows_number, columns_number);
+        } else if (matrix_pointer && option == 6) {
+            clear(matrix_pointer, rows_number);
+        }else {
+            printf("invalid input\n");
         }
+    }
+}
+
+void memory(int *** array_pointer_reference, int rows, int columns) 
+{
+    *array_pointer_reference = (int **)calloc(rows, sizeof(int *));
+    for (size_t i = 0; i < rows; i++)
+    {
+        (*array_pointer_reference)[i] = (int *)calloc(columns, sizeof(int));
+    }
+}
+
+void clear(int ** array_pointer, int rows) 
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        free(array_pointer[i]);
+    }
+    free(array_pointer);
+}
+
+void print(int ** array_pointer, int rows, int columns)
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            printf("%d\t", array_pointer[i][j]);
+        }
+        printf("\n");
     }
 }
