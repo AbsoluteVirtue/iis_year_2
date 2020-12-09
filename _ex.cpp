9a36a842b4fc81d2a0c2c431c475d90d83649f2e
Ivan <3hitemouse@gmail.com>
2020-12-09 14:30:45 +0200

    small tweaks; added se5

--- _ex.cpp
+++ _ex.cpp
@@ -1,11 +1,9 @@
-#include <algorithm>
-#include <cstring>
+
 #include <iostream>
-#include <vector>
 
 int main(int argc, char const *argv[])
 {
-    std::vector<int> v(1);
-    v.push_back(100);
+
+
     return 0;
 }
