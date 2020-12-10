3689656d0ec303b794c784bbd4c40a2bf0df59e2
Leon <leonbrcs@gmail.com>
2020-12-10 17:36:58 +0200

    lab 6

--- _ex.c
+++ _ex.c
@@ -2,34 +2,80 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef unsigned short uint2_t;
 
-struct book
+typedef struct book
 {
     char *author;
     char *title;
-    unsigned short year;
-};
+    uint2_t year;
+} Book;
 
+void fill(Book * lhs, const char * a, const char * t, const uint2_t y);
+int comp(const void * a, const void * b);
+void push_back(Book ** array, const char * a, const char * t, const uint2_t y, size_t idx);
 
 int main(int argc, char const *argv[])
 {
     #define NO_OF_BOOKS 3
 
-    struct book file [NO_OF_BOOKS] = {
+    Book file [NO_OF_BOOKS] = {
         {"Thackeray, William", "The Luck of Barry Lyndon", 1844},
         {"Bronte, Charlotte", "Jane Eyre: An Autobiography", 1847},
         {"Austen, Jane", "Pride and Prejudice", 1813},
     };
 
-    struct book * books;
+    Book * books = (Book *)malloc(NO_OF_BOOKS * sizeof(Book));
+    if (books == NULL){
+        return -1;
+    }
 
     for (size_t i = 0; i < NO_OF_BOOKS; i++)
     {
+        fill(&books[i], file[i].author, file[i].title, file[i].year);
+    
         printf("%s \"%s\" (%d)\n", 
                 books[i].author, 
-                books[i].title, 
+                books[i].title,
                 books[i].year);
     }
 
+    push_back(&books, "Bronte, Emily", "Wuthering Heights", 1850, NO_OF_BOOKS);
+
+    qsort(books, NO_OF_BOOKS + 1, sizeof(Book), comp);
+
+    printf("Sorted array\n");
+    for (size_t i = 0; i < NO_OF_BOOKS + 1; i++)
+    {    
+        printf("%s \"%s\" (%d)\n", 
+                books[i].author, 
+                books[i].title,
+                books[i].year);
+    }
+
+    free(books);
+
     return 0;
+}
+
+void fill(Book * lhs, const char * a, const char * t, const uint2_t y) 
+{
+    lhs->author = strdup(a);
+    lhs->title = strdup(t);
+    lhs->year = y;
+}
+
+int comp(const void * a, const void * b) 
+{
+    return *((Book *)a)->author - *((Book *)b)->author;
+}
+
+void push_back(Book ** array, const char * a, const char * t, const uint2_t y, size_t idx) 
+{
+    *array = (Book *)realloc(*array, (idx + 1) * sizeof(Book));
+    if (*array == NULL) 
+    {
+        exit(-1);
+    }
+    fill(&(*array)[idx], a, t, y);
 }
--- _ex.cpp
+++ _ex.cpp
@@ -1,33 +1,71 @@
+#include <algorithm>
 #include <cstring>
 #include <iostream>
 
-struct book
+typedef unsigned short  uint2_t;
+
+struct Book
 {
-    char *author;
-    char *title;
-    unsigned short year;
+    char *author{nullptr};
+    char *title{nullptr};
+    uint2_t year{2000};
+
+    void fill(const char * a, const char * t, const uint2_t y) 
+    {
+        author = strdup(a);
+        title = strdup(t);
+        year = y;
+    }
+    void print() 
+    {
+        std::cout   << author << " "
+                    << title << " "
+                    << year << std::endl;
+    }
+    static bool comp(const Book & lhs, const Book & rhs)
+    {
+        return *lhs.author < *rhs.author;
+    }
 };
 
+void push_back(Book * & array, const char * a, const char * t, const uint2_t y, std::size_t idx) 
+{
+    Book * tmp = new Book [idx + 1];
+    std::copy(array, array + idx, tmp);
+    delete [] array;
+    tmp[idx].fill(a, t, y);
+    array = tmp;
+}
 
 int main(int argc, char const *argv[])
 {
     #define NO_OF_BOOKS 3
 
-    book file [NO_OF_BOOKS] = {
+    Book file [NO_OF_BOOKS] = {
         {"Thackeray, William", "The Luck of Barry Lyndon", 1844},
         {"Bronte, Charlotte", "Jane Eyre: An Autobiography", 1847},
         {"Austen, Jane", "Pride and Prejudice", 1813},
     };
 
-    book * books;
+    Book * books = new Book [NO_OF_BOOKS];
 
     for (size_t i = 0; i < NO_OF_BOOKS; i++)
     {
-        std::cout   << books[i].author
-                    << books[i].title 
-                    << books[i].year << std::endl;
+        books[i].fill(file[i].author, file[i].title, file[i].year);
+        books[i].print();
     }
+
+    push_back(books, "Bronte, Emily", "Wuthering Heights", 1850, NO_OF_BOOKS);
     
+    std::sort(books, books + NO_OF_BOOKS + 1, Book::comp);
+
+    std::cout << "Sorted array\n";
+    for (size_t i = 0; i < NO_OF_BOOKS + 1; i++)
+    {
+        books[i].print();
+    }
+
+    delete [] books;
 
     return 0;
 }
