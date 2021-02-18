5cfd9e6c34053ef0c3f8bfcdde3dd746ae162d16
Leon <leonbrcs@gmail.com>
2021-02-18 10:27:12 +0200

    new file se2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 struct record
 {
@@ -12,26 +13,53 @@ struct record
 
 static size_t count = 10;
 
+void print(record ** & arr, size_t n);
+void create(record ** & arr, size_t n);
+void clear(record ** & arr, size_t n);
+
 int main(int argc, char const *argv[])
 {
-    record ** array = new record * [count];
+    record ** array = nullptr;
 
-    for (size_t i = 0; i < count; i++)
-    {
-        array[i] = new record({
-            "Test",
-            "test",
-            ((double)i + 1) * 10,
-            ((double)i + 1) * 100,
-        });
-    }
+    create(array, count);
 
-    for (size_t i = 0; i < count; i++)
-    {
-        delete array[i];
-    }
-    delete [] array;
+    print(array, count);
+
+    clear(array, count);
 
     std::cout << "Done!";
     return 0;
+}
+
+void create(record ** & arr, size_t n)
+{
+    arr = new record * [n];
+
+    for (size_t i = 0; i < n; i++)
+    {
+        arr[i] = new record;
+        arr[i]->birthplace = "Hello " + i;
+        std::ostringstream addr;
+        addr << arr[i];
+        arr[i]->code = addr.str();
+        arr[i]->night_pop = 1;
+        arr[i]->resident_pop = 2;
+    }
+}
+
+void clear(record ** & arr, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        delete arr[i];
+    }
+    delete [] arr;
+}
+
+void print(record ** & arr, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cout << arr[i]->code << "\n";
+    }
 }
