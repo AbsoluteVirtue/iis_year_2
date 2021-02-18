8d488665a7c31cf78309bc088b0c6ebb54c7f3cf
Ivan <3hitemouse@gmail.com>
2021-02-18 18:20:22 +0200

    tweaks

--- _ex.cpp
+++ _ex.cpp
@@ -7,9 +7,37 @@
 
 #include "smstr2/si_202_ghim/flight_funs.h"
 
+struct vec
+{
+    int x{0}, y{0};
+
+    vec() {}
+
+    vec(const vec & other)
+    {
+        this->x = this->y = 5;
+    }
+
+    vec & operator=(const vec & other)
+    {
+        this->x = this->y = 4;
+        return *this;
+    }
+    vec & operator=(vec && other) noexcept 
+    {
+        this->x = this->y = 3;
+        return *this;
+    }
+};
+
 int main(int argc, char const *argv[])
 {
-    
+    vec a = vec();
+    a.x = 1;
+    a.y = 1;
+    vec b = a;
+
+    std::cout << b.x << " " << b.y;
 
     return 0;
 }
