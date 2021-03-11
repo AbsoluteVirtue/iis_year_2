d79a77f2d46273f0dbb8ea8c03ad5d09bb272fe1
Ivan <3hitemouse@gmail.com>
2021-03-11 12:42:57 +0200

    removed redundant files

--- smstr2/lw8.cpp
+++ smstr2/lw8.cpp
@@ -160,6 +160,13 @@ node * merge_sorted(node * left, node * right)
     return placeholder->next;
 }
 
+// http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.31.9981
+// Treesort is best, followed by Quicksort and Mergesort. 
+// Sediment sort, bubble sort, selection sort perform very badly.
+
+// В зависимости от "железа" иногда быстрее скопировать список в массив и применить быстрый алгоритм сортировки, чем сортировать с помощью медленных алгоритмов.
+// https://www.chiark.greenend.org.uk/~sgtatham/algorithms/listsort.html
+
 // https://zxi.mytechroad.com/blog/divide-and-conquer/leetcode-148-sort-list/
 
 node * merge_sort(node * head)
--- smstr2/se5.cpp
+++ smstr2/se5.cpp
@@ -1,19 +1,83 @@
-// http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.31.9981
-// His empirical conclusion is that Treesort is best, followed by Quicksort and Mergesort. 
-// Sediment sort, bubble sort, selection sort perform very badly.
-
-// В зависимости от "железа" иногда быстрее скопировать список в массив и применить быстрый алгоритм сортировки, чем сортировать с помощью медленных алгоритмов.
-// https://www.chiark.greenend.org.uk/~sgtatham/algorithms/listsort.html
-
 #include <iostream>
-#include <string>
-#include <cstdlib>
+#include <queue>
+#include <stack>
+
+// 1. vertex visitation - 2. vertex exploration
+// Breadth-first search; on binary trees: level-ordered traversal / queue-based; BFS spanning tree w/ cross-edges
+// Depth-first search; on binary trees: pre-ordered traversal / stack-based; DFS spanning tree w/ back-edges
+
+#define N 7
+
+enum node_status
+{
+	UNKNOWN,
+	DISCOVERED,
+	VISITED,
+};
 
 int main(int argc, char const *argv[])
 {
-    std::string * arr = (std::string *)malloc(10 * sizeof(std::string));
-    arr[0] = "test";
-    std::cout << arr[0];
-    free(arr); 
-    return 0;
+	bool adj_matrix[N][N] = {
+		{0, 1, 1, 0, 0, 0, 1},
+		{1, 0, 1, 1, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0},
+		{0, 1, 0, 0, 1, 0, 0},
+		{0, 0, 0, 1, 0, 1, 0},
+		{0, 0, 0, 0, 1, 0, 1},
+		{1, 0, 0, 0, 0, 1, 0},
+	};
+
+	node_status nodes[N] = {};
+
+	std::queue<int> Q;
+
+	Q.push(0);
+
+	while (!Q.empty())
+	{
+		int current = Q.front();
+		Q.pop();
+
+		nodes[current] = VISITED;
+		for (size_t i = 0; i < N; i++)
+		{
+			if (adj_matrix[current][i] && nodes[i] == UNKNOWN)
+			{
+				Q.push(i);
+				nodes[i] = DISCOVERED;
+			}
+		}
+		std::cout << current << "\t";
+	}
+
+	std::cout << "\n";
+
+	for (size_t i = 0; i < N; i++)
+	{
+		nodes[i] = UNKNOWN;
+	}
+
+	std::stack<int> S;
+
+	S.push(0);
+	while (!S.empty())
+	{
+		int current = S.top();
+		S.pop();
+
+		if (nodes[current] == VISITED) continue;
+		
+		nodes[current] = VISITED;
+		for (int i = N - 1; i >= 0; i--)
+		{
+			if (adj_matrix[current][i] && nodes[i] != VISITED)
+			{
+				S.push(i);
+				nodes[i] = DISCOVERED;
+			}
+		}
+		std::cout << current << "\t";
+	}
+
+	return 0;
 }
